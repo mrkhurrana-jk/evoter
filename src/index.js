010088c692b2e3fa72d17c0b1ee5c80532e9d075
mrkhurrana-jk <jatinkhurana2410@.com>
2023-01-27 15:00:39 +0530

    solved error in displaying home page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
-ReactDOM.render(
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+),
   <React.StrictMode>
     <App />
   </React.StrictMode>,
